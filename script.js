238865e1797291dd78070856d6099c09c605b546
lianaschapiro <liana.schapiro@gmail.com>
2015-12-11 14:50:21 -0500

    Added toggling images that link to different links

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
-	$(".lightbox-link").on("click", function(){
+	$(".lightbox-link").on("click", function(e){
+		e.preventDefault();
+		var $image = $("#chosen-image");
+		$image.attr("src", $(this). attr("src"));
 		$(".outer-lightbox").show();
+			$(".outer-lightbox").on("click", function(){
+				$(".outer-lightbox").hide();
+		});
 	});
-
-	$(".outer-lightbox").on("click", function(){
-		$(".outer-lightbox").hide();
-	});
-
 });
