6ea787d25538fcc2465145ce2b26f402f199d8ba
lianaschapiro <liana.schapiro@gmail.com>
2015-12-11 15:49:36 -0500

    Added image captions

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 	$(".lightbox-link").on("click", function(e){
 		e.preventDefault();
-		var $image = $("#chosen-image");
-		$image.attr("src", $(this). attr("src"));
+		$("#text-box").text($(this).attr("monkey"));
+		$("#chosen-image").attr("src", $(this). attr("src"));
 		$(".outer-lightbox").show();
 			$(".outer-lightbox").on("click", function(){
 				$(".outer-lightbox").hide();
