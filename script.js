db04c027c32e3906119d026d192c67d0c84bbb69
lianaschapiro <liana.schapiro@gmail.com>
2015-12-11 11:53:12 -0500

    Added show/hide properties to outer lightbox div

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
+	$(".lightbox-link").on("click", function(){
+		$(".outer-lightbox").show();
+	});
 
-
-
-
-
+	$(".outer-lightbox").on("click", function(){
+		$(".outer-lightbox").hide();
+	});
 
 });
